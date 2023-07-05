1ad6dfdd537d4661beccf149164556d676e6aded
TANAYA <tanayapawar530@gmail.com>
2023-07-05 04:03:43 -0700

    welcome to Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>HELLO WORLD!</h1>'
+})
+export class AppComponent {
+  title = 'angulardemo';
+}
